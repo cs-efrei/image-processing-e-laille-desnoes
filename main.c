fa58132bc6a830f9100bd22720435caa14d2f955
marine <marine.laille@efrei.net>
2025-03-28 15:41:20 +0100

    Create README.md

--- main.c
+++ main.c
@@ -1,11 +1,43 @@
-//
-// Created by marin on 27/03/2025.
-//
-
-#include <stdio.h>
+// main.c - Interface en ligne de commande
+#include "t_bmp8.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char filename[256];
+    int choice;
+    t_bmp8 *img = NULL;
+
+    while (1) {
+        printf("\n1. Charger une image\n2. Sauvegarder une image\n3. Appliquer un négatif\n4. Modifier la luminosité\n5. Seuillage\n6. Quitter\nVotre choix : ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("Entrez le nom du fichier : ");
+            scanf("%s", filename);
+            img = bmp8_loadImage(filename);
+        } else if (choice == 2 && img) {
+            printf("Nom du fichier de sortie : ");
+            scanf("%s", filename);
+            bmp8_saveImage(filename, img);
+        } else if (choice == 3 && img) {
+            bmp8_negative(img);
+        } else if (choice == 4 && img) {
+            int value;
+            printf("Valeur de luminosité (+/-) : ");
+            scanf("%d", &value);
+            bmp8_brightness(img, value);
+        } else if (choice == 5 && img) {
+            int threshold;
+            printf("Valeur de seuil : ");
+            scanf("%d", &threshold);
+            bmp8_threshold(img, threshold);
+        } else if (choice == 6) {
+            if (img) bmp8_free(img);
+            break;
+        } else {
+            printf("Option invalide ou image non chargée.\n");
+        }
+    }
     return 0;
 }
+
 
---
+++ t_bmp8.c
@@ -0,0 +1,77 @@
+// bmp8.c - Implémentation des fonctions
+#include "bmp8.h"
+
+t_bmp8* bmp8_loadImage(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (!file) {
+        printf("Erreur : Impossible d'ouvrir le fichier %s\n", filename);
+        return NULL;
+    }
+
+    t_bmp8 *img = (t_bmp8 *)malloc(sizeof(t_bmp8));
+    fread(img->header, sizeof(unsigned char), 54, file);
+    img->width = *(int*)&img->header[18];
+    img->height = *(int*)&img->header[22];
+    img->dataSize = img->width * img->height;
+    fread(img->colorTable, sizeof(unsigned char), 1024, file);
+    img->data = (unsigned char *)malloc(img->dataSize);
+    fread(img->data, sizeof(unsigned char), img->dataSize, file);
+    fclose(file);
+    return img;
+}
+
+void bmp8_saveImage(const char *filename, t_bmp8 *img) {
+    FILE *file = fopen(filename, "wb");
+    fwrite(img->header, sizeof(unsigned char), 54, file);
+    fwrite(img->colorTable, sizeof(unsigned char), 1024, file);
+    fwrite(img->data, sizeof(unsigned char), img->dataSize, file);
+    fclose(file);
+}
+
+void bmp8_free(t_bmp8 *img) {
+    free(img->data);
+    free(img);
+}
+
+void bmp8_printInfo(t_bmp8 *img) {
+    printf("Width: %d\n", img->width);
+    printf("Height: %d\n", img->height);
+    printf("Data Size: %d\n", img->dataSize);
+}
+
+void bmp8_negative(t_bmp8 *img) {
+    for (int i = 0; i < img->dataSize; i++) {
+        img->data[i] = 255 - img->data[i];
+    }
+}
+
+void bmp8_brightness(t_bmp8 *img, int value) {
+    for (int i = 0; i < img->dataSize; i++) {
+        int temp = img->data[i] + value;
+        img->data[i] = (temp > 255) ? 255 : (temp < 0) ? 0 : temp;
+    }
+}
+
+void bmp8_threshold(t_bmp8 *img, int threshold) {
+    for (int i = 0; i < img->dataSize; i++) {
+        img->data[i] = (img->data[i] >= threshold) ? 255 : 0;
+    }
+}
+
+void bmp8_applyFilter(t_bmp8 img, float kernel[3][3]) {
+    unsigned charnewData = (unsigned char )malloc(img->dataSize);
+    for (int y = 1; y < img->height - 1; y++) {
+        for (int x = 1; x < img->width - 1; x++) {
+            float sum = 0;
+            for (int j = -1; j <= 1; j++) {
+                for (int i = -1; i <= 1; i++) {
+                    sum += img->data[(y + j) img->width + (x + i)] * kernel[j + 1][i + 1];
+                }
+            }
+            newData[y * img->width + x] = (sum > 255) ? 255 : (sum < 0) ? 0 : (unsigned char)sum;
+        }
+    }
+    free(img->data);
+    img->data = newData;
+}
+
